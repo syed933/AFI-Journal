2aba7807e6b2cbe71d97a155f7b99ab53ebb0bf7
Syed <syehassan@gmail.com>
2022-03-16 16:29:06 -0700

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'afi-journal',
+    appId: '1:430531002576:web:a9d8e77e64593768265ed5',
+    storageBucket: 'afi-journal.appspot.com',
+    apiKey: 'AIzaSyD_wFxstVAwLUEwtri8snuIcNv_D9MGfLM',
+    authDomain: 'afi-journal.firebaseapp.com',
+    messagingSenderId: '430531002576',
+  },
   production: true
 };
